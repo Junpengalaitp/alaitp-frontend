1873df559dd30f4d0ac2530d91fb998e5ccd1ba4
Junpeng He <hejunpeng2012@hotmail.com>
2019-11-09 19:34:29 +0800

    styling keyword

---
+++ src/components/Keyword.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Keyword = props => {
+  return (
+    <div>
+      <a href="/" className="btn btn-outline-info">{props.children}</a>
+    </div>
+  )
+}
--- src/components/Keywords.js
+++ src/components/Keywords.js
@@ -6,27 +6,27 @@ const Keywords = props => {
   
 
   const programmingLanguagesItems = props.keywords === null? null : props.keywords.programmingLanguage.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const libraryOrFrameworksItems = props.keywords === null? null : props.keywords.libraryOrFramework.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const divisionsItems = props.keywords === null? null : props.keywords.division.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const dataStoragesItems = props.keywords === null? null : props.keywords.dataStorage.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const platformsItems = props.keywords === null? null : props.keywords.platform.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const approachesItems = props.keywords === null? null : props.keywords.approach.map((keyword, index) => (
-    <li key={index}>{keyword}</li>
+    <a href='/' className="list-group-item list-group-item-primary" key={index}>{keyword}</a>
   ))
 
   const searchResult = props.loading ? (
@@ -34,45 +34,45 @@ const Keywords = props => {
   ) : (
     <div className="row">
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-info btn-sm'>Programming Language</small>
+        <div className="list-group text-center list-unstyled list-group-flush">
+          <a href='/' className='list-group-item list-group-item-info'>Programming Language</a>
           {programmingLanguagesItems}
-        </ul>
+        </div>
       </div>
 
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-success btn-sm'>Library and Framework</small>
+        <div className="list-group text-center list-unstyled">
+          <a href='/' className='list-group-item list-group-item-success'>Library and Framework</a>
           {libraryOrFrameworksItems}
-        </ul>
+        </div>
       </div>
 
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-danger btn-sm'>Division</small>
+        <div className="list-group text-center list-unstyled">
+          <a href='/' className='list-group-item list-group-item-danger'>Division</a>
           {divisionsItems}
-        </ul>
+        </div>
       </div>
 
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-warning btn-sm'>Platform</small>
+        <div className="list-group text-center list-unstyled">
+          <a href='/' className='list-group-item list-group-item-warning'>Platform</a>
           {platformsItems}
-        </ul>
+        </div>
       </div>
 
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-secondary btn-sm'>Approach</small>
+        <div className="list-group text-center list-unstyled">
+          <a href='/' className='list-group-item list-group-item-light'>Approach</a>
           {approachesItems}
-        </ul>
+        </div>
       </div>
 
       <div className="col-sm-2">
-        <ul className="text-center list-unstyled">
-          <small className='btn btn-dark btn-sm'>Data Storage</small>
+        <div className="list-group text-center list-unstyled">
+          <a href='/' className='list-group-item list-group-item-dark'>Data Storage</a>
           {dataStoragesItems}
-        </ul>
+        </div>
       </div>
 
     </div>
