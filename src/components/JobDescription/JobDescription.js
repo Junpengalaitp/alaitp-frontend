f3c5d1ca10796319b2198fb2f1d890eccca90f1e
hejp009 <hejunpeng2012@hotmail.com>
2019-11-21 13:51:02 +0800

    bootstarp-react init and job descirtion collapse

--- src/components/JobDescription/JobDescription.js
+++ src/components/JobDescription/JobDescription.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Collapse, Card } from 'react-bootstrap';
 
 const JobDescription = props => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="card border-dark mb-1" style={{maxWidth: '32rem'}}>
-      <div className="card-header blockquote">{props.title}</div>
-      <div className="card-body text-dark clearfix">
-        <p className="card-title float-left"><strong>{props.company}</strong></p>
-        <p className="card-text float-right">{props.tags}</p>
-      </div>
-    </div>
+    <Card style={{ width: '32rem' }}>
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted clearfix">
+          <p className="card-title float-left"><strong>{props.company}</strong></p>
+          <p className="card-text float-right">{props.tags}</p>
+        </Card.Subtitle>
+        <Button
+          variant="outline-info btn-sm"
+          onClick={() => setOpen(!open)}
+          aria-controls="job-collapse-text"
+          aria-expanded={open}
+        >See Job Text</Button>
+        <Card.Text>
+          <Collapse in={open}>
+            <div id="job-collapse-text mx-1">
+              {props.jobDescriptionText}
+            </div>
+          </Collapse>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
 
--- src/components/JobDescription/JobDescriptionList.js
+++ src/components/JobDescription/JobDescriptionList.js
@@ -1,48 +1,42 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import JobDescription from './JobDescription';
-import { searchJobs } from '../../store/actions/jobSearch';
-import Spinner from './../UI/Spinner/Spinner';
+import JobDescription from "./JobDescription";
+import { searchJobs } from "../../store/actions/jobSearch";
+import Spinner from "./../UI/Spinner/Spinner";
 
 class JobDescriptionList extends React.Component {
-
   componentDidMount() {
-    this.props.onKeywordSearchSuccess("Software Engineer")
-    console.log(this.props.jobList)
+    this.props.onKeywordSearchSuccess("Software Engineer");
+    console.log(this.props.jobList);
   }
 
   render() {
-    // const searchResult = this.props.loading? <Spinner /> : (
-    //   <div>
-    //     <JobDescription title={this.props.jobList[0].jobTitle} company={this.props.jobList[0].company} tags={this.props.jobList[0].tags} />
-    //     <JobDescription title={this.props.jobList[1].jobTitle} company={this.props.jobList[1].company} tags={this.props.jobList[1].tags}/>
-    //     <JobDescription title={this.props.jobList[2].jobTitle} company={this.props.jobList[2].company} tags={this.props.jobList[2].tags}/>
-    //     <JobDescription title={this.props.jobList[3].jobTitle} company={this.props.jobList[3].company} tags={this.props.jobList[3].tags}/>
-    //     <JobDescription title={this.props.jobList[4].jobTitle} company={this.props.jobList[4].company} tags={this.props.jobList[4].tags}/>
-    //     <JobDescription title={this.props.jobList[5].jobTitle} company={this.props.jobList[5].company} tags={this.props.jobList[5].tags}/>
-    //   </div>
-    //   )
-    let searchResult = <Spinner />
+    let searchResult = <Spinner />;
     if (!this.props.loading) {
-      searchResult = this.props.jobList.slice(0, 6).map(jobDescription => (
-        <JobDescription title={jobDescription.jobTitle} company={jobDescription.company} tags={jobDescription.tags} key={jobDescription.jobId} />
-      ))
+      searchResult = this.props.jobList
+        .slice(0, 6)
+        .map(jobDescription => (
+          <JobDescription
+            title={jobDescription.jobTitle}
+            company={jobDescription.company}
+            tags={jobDescription.tags}
+            key={jobDescription.jobId}
+            jobDescriptionText={jobDescription.jobDescriptionText}
+          />
+        ));
     }
 
-    return (
-      <React.Fragment>
-        {searchResult}
-      </React.Fragment>
-  )}
+    return <React.Fragment>{searchResult}</React.Fragment>;
+  }
 }
 
 const mapStateToProps = state => {
   return {
     jobList: state.jobDescription.jobList,
     loading: state.jobDescription.loading
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -50,4 +44,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(JobDescriptionList)
+export default connect(mapStateToProps, mapDispatchToProps)(JobDescriptionList);
