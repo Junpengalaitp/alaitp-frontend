ade1003072ec18e2d2eeeee31a35e0faf83018d0
hejp009 <hejunpeng2012@hotmail.com>
2019-11-21 14:01:37 +0800

    migrate react-bootstrap SearchBar

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Route } from 'react-router'
 import SearchForm from "./SearchForm";
+import { Container, Row, Col } from "react-bootstrap";
 
 const SearchBar = () => {
 
   return (
-    <div>
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-xl-9 mx-auto">
-            <h1 className="mb-5">Search For Most Wanted Technical Skills!</h1>
-          </div>
-          <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <Route path='/' exact render={(props) => <SearchForm {...props} inputCSS="form-control form-control-lg" buttonCSS="btn btn-outline-primary btn-lg" />} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <Container className="mt-5">
+      <Row>
+        <Col className="col-xl-9 mx-auto">
+          <h1 className="mb-5">Search For Most Wanted Technical Skills!</h1>
+        </Col>
+        <Col className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <Route path='/' exact render={(props) => <SearchForm {...props} inputCSS="form-control form-control-lg" buttonCSS="btn btn-outline-primary btn-lg" />} />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
 
 export default SearchBar
