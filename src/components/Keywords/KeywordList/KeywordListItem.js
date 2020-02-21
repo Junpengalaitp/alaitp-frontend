da5391de5e72222bfabe22333d946faaa6553e12
Junpeng He <hejunpeng2012@hotmail.com>
2020-02-21 22:23:07 +0800

    remove duplicated keywords and sort by frequency

--- src/components/Keywords/KeywordList/KeywordListItem.js
+++ src/components/Keywords/KeywordList/KeywordListItem.js
@@ -4,14 +4,25 @@ import { ListGroup } from 'react-bootstrap'
 
 const KeywordListItem = props => {
   if (props.keywords === null) return null
-  const keywordInCategory = props.keywords[props.keywordType]
+  let keywordInCategory = props.keywords[props.keywordType]
   if (keywordInCategory === null || keywordInCategory === undefined) return null
   
+  const keywordFreq = {}
+  for (const keyword of keywordInCategory) {
+    if (!keywordInCategory.hasOwnProperty) {
+      keywordFreq[keyword] = 1
+    } else {
+      keywordFreq[keyword]++
+    }
+  }
+  keywordInCategory = [...new Set(keywordInCategory)]
   // sort keywords by its count, DESC
   keywordInCategory.sort((a, b) => {
-    return keywordInCategory[b] - keywordInCategory[a]
+    return keywordFreq[b] - keywordFreq[a]
   })
 
+  console.log("keywordInCategory")
+  console.log(keywordInCategory)
   const keywordListItem = keywordInCategory.slice(0, 6).map((keyword, index) => (
       <ListGroup.Item href='/' variant="light" key={index}>{keyword}</ListGroup.Item>
   ))
