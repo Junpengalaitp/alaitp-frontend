d8902658e5d351e9e149f81ae7a0a431da7e745e
hejp009 <hejunpeng2012@hotmail.com>
2020-03-02 11:22:12 +0800

    fix route

--- src/components/UI/NavigationBar.js
+++ src/components/UI/NavigationBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     <Row className="justify-content-md-center">
       <Col xs={10}>
         <Navbar sticky="top" bg="light" variant="light" expand="lg" className="justify-content-between mb-5">
-          <Navbar.Brand href="/">ALAITP</Navbar.Brand>
+          <Navbar.Brand href="/alaitp-frontend">ALAITP</Navbar.Brand>
           <Route path='/alaitp-frontend/keywords' render={(props) => <SearchForm {...props} inputCSS="mr-sm-2" buttonCSS="btn btn-outline-primary" show />} />
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
