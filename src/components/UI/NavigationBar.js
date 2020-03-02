19a7cbeff1fc267eebdc39125431f720c9fed822
hejp009 <hejunpeng2012@hotmail.com>
2020-03-02 11:18:31 +0800

    fix navbar routes

--- src/components/UI/NavigationBar.js
+++ src/components/UI/NavigationBar.js
@@ -9,14 +9,14 @@ const NavBar = () => {
       <Col xs={10}>
         <Navbar sticky="top" bg="light" variant="light" expand="lg" className="justify-content-between mb-5">
           <Navbar.Brand href="/">ALAITP</Navbar.Brand>
-          <Route path='/keywords' render={(props) => <SearchForm {...props} inputCSS="mr-sm-2" buttonCSS="btn btn-outline-primary" show />} />
+          <Route path='/alaitp-frontend/keywords' render={(props) => <SearchForm {...props} inputCSS="mr-sm-2" buttonCSS="btn btn-outline-primary" show />} />
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/">About</Nav.Link>
-              <Nav.Link href="/">Services</Nav.Link>
-              <Nav.Link href="/">Contact</Nav.Link>
+              <Nav.Link href="/alaitp-frontend">Home</Nav.Link>
+              <Nav.Link href="/alaitp-frontend">About</Nav.Link>
+              <Nav.Link href="/alaitp-frontend">Services</Nav.Link>
+              <Nav.Link href="/alaitp-frontend">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
