c9127c56419d5f25d8005f9babb438e413c612e7
Junpeng He <hejunpeng2012@hotmail.com>
2020-04-06 21:24:56 +0800

    change search result

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,21 +1,20 @@
-import React from "react";
-import JobDescriptionList from '../JobDescription/JobDescriptionList';
-import { Col, Row } from 'react-bootstrap';
-import KeywordListGroup from "../Keywords/KeywordList/KeywordListGroup";
+import React from "react"
+import JobDescriptionList from '../JobDescription/JobDescriptionList'
+import KeywordListGroup from "../Keywords/KeywordList/KeywordListGroup"
 
 
 class SearchResult extends React.Component {
   
   render() {
     const searchResult = (
-      <Row className="justify-content-md-center">
-        <Col xs={5}>
+      <div className="row justify-content-center">
+        <div className="col-sm-5">
           <KeywordListGroup />
-        </Col>
-        <Col xs={5}>
+        </div>
+        <div className="col-sm-5">
           <JobDescriptionList />
-        </Col>    
-      </Row>
+        </div>    
+      </div>
     )
     return searchResult
   }
