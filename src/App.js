3d097e152d8ae64ff14681dece1cd4fa7d524419
Junpeng He <hejunpeng2012@hotmail.com>
2020-04-07 21:11:19 +0800

    make sass working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.scss'
 
 import Layout from './containers/Layout/Layout'
 
--- src/components/Keywords/KeywordList/KeywordListItem.js
+++ src/components/Keywords/KeywordList/KeywordListItem.js
@@ -5,6 +5,10 @@ import * as actionTypes from '../../../store/actions/actionTypes'
 import { CoOccurrencePopover } from '../CoOccurredWord/CoOccurrencePopover';
 import { OverlayTrigger } from 'react-bootstrap';
 
+const style = {
+  width: "120px",
+  height: "80px"
+}
 
 class KeywordListItem extends React.Component {
 
@@ -18,9 +22,8 @@ class KeywordListItem extends React.Component {
     
     if (keywordInCategory === null || keywordInCategory === undefined) return null
 
-
     const keywordListItem = keywordInCategory.slice(0, 8).map((keyword, index) => (
-      <li className="list-group-item list-group-item-light" key={index} action>
+      <li className="list-group-item list-group-item-light" style={style} key={index} action>
         <OverlayTrigger trigger="click" placement="right" overlay={<CoOccurrencePopover>{keyword}</CoOccurrencePopover>} rootClose={true} >
           <p>{keyword}</p>
         </OverlayTrigger>
