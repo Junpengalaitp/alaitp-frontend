fdf0a5edb4b142a4048b5445ddf46e92048cec1d
Junpeng He <hejunpeng2012@hotmail.com>
2020-07-30 21:25:16 +0800

    feat(person page): contact bar icons

--- src/components/PersonalPage/ContactBar.js
+++ src/components/PersonalPage/ContactBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import wechat from "./resource/image/wechat.png"
+import linkedin from "./resource/image/linkedin.png"
+import github from "./resource/image/github.png"
+import leetcode from "./resource/image/leetcode.png"
 
 const ContactBar = () => (
   <div className="contact-bar">
-    <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
-      <div className="btn-group" role="group" aria-label="First group">
-        <button type="button" className="btn btn-secondary">1</button>
-        <button type="button" className="btn btn-secondary">2</button>
-        <button type="button" className="btn btn-secondary">3</button>
-        <button type="button" className="btn btn-secondary">4</button>
-      </div>
+    <div className="btn-group">
+      <button type="button">
+        <img src={wechat} alt=""/>
+      </button>
+      <button type="button">
+        <img src={linkedin} alt="https://www.linkedin.com/in/junpeng-he"/>
+      </button>
+      <button type="button">
+        <img src={github} alt=""/>
+      </button>
+      <button type="button">
+        <img src={leetcode} alt=""/>
+      </button>
     </div>
   </div>
 );
