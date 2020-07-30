f8b8c6984e27f7a974fa63d6556cf5c7406a6980
Junpeng.He <hejunpeng2012@hotmail.com>
2020-07-30 13:09:48 +0800

    feat(personal page): ContactBar init

---
+++ src/components/PersonalPage/ContactBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactBar = () => (
+  <div className="contact-bar">
+    <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+      <div className="btn-group" role="group" aria-label="First group">
+        <button type="button" className="btn btn-secondary">1</button>
+        <button type="button" className="btn btn-secondary">2</button>
+        <button type="button" className="btn btn-secondary">3</button>
+        <button type="button" className="btn btn-secondary">4</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ContactBar;
--- src/containers/Layout/PersonalPage.js
+++ src/containers/Layout/PersonalPage.js
@@ -3,6 +3,7 @@ import NavBar from "../../components/UI/NavBar";
 import SideNav from "../../components/PersonalPage/SideNav";
 import MediaContent from "../../components/PersonalPage/MediaContent";
 import Avatar from "../../components/PersonalPage/avatar";
+import ContactBar from "../../components/PersonalPage/ContactBar";
 
 const PersonalPage = () => {
   return (
@@ -12,6 +13,7 @@ const PersonalPage = () => {
         <div className="col-1">
           <SideNav/>
           <Avatar/>
+          <ContactBar />
         </div>
         <div className="col-9">
           <MediaContent/>
