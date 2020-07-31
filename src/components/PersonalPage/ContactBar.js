883c62003df99a2da0b94ec5fa69e3e8d6eb36c2
Junpeng.He <hejunpeng2012@hotmail.com>
2020-07-31 12:36:25 +0800

    feat(personal page): email popover working

--- src/components/PersonalPage/ContactBar.js
+++ src/components/PersonalPage/ContactBar.js
@@ -3,10 +3,21 @@ import wechat from "./resource/image/wechat.png"
 import linkedin from "./resource/image/linkedin.png"
 import github from "./resource/image/github.png"
 import leetcode from "./resource/image/leetcode.png"
+import email from "./resource/image/email.png"
 import WechatQRModal from "./WechatQRModal";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
 const ContactBar = () => {
   const [modalShow, setModalShow] = useState(false)
+
+  const emailPopover = (
+    <Popover id="popover-basic">
+      <Popover.Content>
+        hejunpeng2012@hotmail.com
+      </Popover.Content>
+    </Popover>
+  );
 
   return (
     <div className="contact-bar">
@@ -25,6 +36,11 @@ const ContactBar = () => {
         </button>
         <button type="button">
           <img src={leetcode} alt=""/>
+        </button>
+        <button type="button">
+          <OverlayTrigger trigger="click" placement="right" overlay={emailPopover}>
+            <img src={email} alt=""/>
+          </OverlayTrigger>
         </button>
       </div>
     </div>
