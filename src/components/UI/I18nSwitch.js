b1fdc10584132d2bb04bc9c713ff19b1e5c20464
Junpeng He <hejunpeng2012@hotmail.com>
2020-10-06 17:06:23 +0800

    feat(mobile): mobile i18n position

--- src/components/UI/I18nSwitch.js
+++ src/components/UI/I18nSwitch.js
@@ -4,8 +4,9 @@ import * as actionTypes from "../../store/actions/actionTypes";
 
 const I18NSwitch = props => {
   const text = props.language === "en" ? "中文" : "English"
+  const width = window.innerWidth;
   return (
-    <div className="leftMid">
+    <div className={width < 992 ? "midBottom" : "leftMid"}>
       <button type="button" className="btn btn-info btn-lg btn-circle"
               onClick={() => props.switchLang()}>{text}</button>
     </div>
